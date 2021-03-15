6ac7324054085d0079c73762cf83023decab8ba4
Halakhamayseh <khamaysehhala95@yahoo.com>
2021-03-15 23:17:41 +0200

    fixed js code ,i add button event in js and if statment to void repatation

--- js/app.js
+++ js/app.js
@@ -21,10 +21,10 @@ const nameOfProducts=[
   'water-can.jpg',
   'wine-glass.jpg',
 ];
-const imageSection=document.getElementById('imageSection');
-const left=document.getElementById('left');
-const middle=document.getElementById('middle');
-const right=document.getElementById('right');
+let imageSection=document.getElementById('imageSection');
+let left=document.getElementById('left');
+let middle=document.getElementById('middle');
+let right=document.getElementById('right');
 function BusMall(nameOfProducts,imgEx){
   this.nameOfProducts=nameOfProducts;
   this.imgEx=imgEx;
@@ -38,45 +38,70 @@ for ( let i =0; i<nameOfProducts.length;i++){
   new BusMall (nameOfProducts[i]);
 }
 function render (){
-  const leftProduct= randomNumber(0,BusMall.all.length-1);
-  const leftRandom=BusMall.all[leftProduct];
-  left.src=leftRandom.src;
-  left.title=leftRandom.nameOfProducts;
-  left.alt=leftRandom.nameOfProducts;
-  const middleProduct= randomNumber(0,BusMall.all.length-1);
-  const middleRandom=BusMall.all[middleProduct];
-  middle.src=middleRandom.src;
-  middle.title=middleRandom.nameOfProducts;
-  middle.alt=middleRandom.nameOfProducts;
-  const rightProduct= randomNumber(0,BusMall.all.length-1);
-  const rightRandom=BusMall.all[rightProduct];
-  right.src=rightRandom.src;
-  right.title=rightRandom.nameOfProducts;
-  right.alt=rightRandom.nameOfProducts;
+  //max,min random and put the data in array(left)//
+  let leftProduct= randomNumber(0,BusMall.all.length-1);
+  let leftRandom=BusMall.all[leftProduct];
+  //max,min random and put the data in array(middle)//
+  let middleProduct= randomNumber(0,BusMall.all.length-1);
+  let middleRandom=BusMall.all[middleProduct];
+  //max,min random and put the data in array(right)//
+  let rightProduct= randomNumber(0,BusMall.all.length-1);
+  let rightRandom=BusMall.all[rightProduct];
+  //if for repeated//
+  if (rightRandom!== leftRandom&& middleRandom&&leftRandom!==middleRandom){
+    //left//
+    left.src=leftRandom.src;
+    left.title=leftRandom.nameOfProducts;
+    left.alt=leftRandom.nameOfProducts;
+    //middle//
+    middle.src=middleRandom.src;
+    middle.title=middleRandom.nameOfProducts;
+    middle.alt=middleRandom.nameOfProducts;
+    //right//
+    right.src=rightRandom.src;
+    right.title=rightRandom.nameOfProducts;
+    right.alt=rightRandom.nameOfProducts;
+    //increase the viwes for each side//
+    BusMall.all[rightProduct].views++;
+    BusMall.all[leftProduct].views++;
+    BusMall.all[middleProduct].views++;
+  }
 }
 imageSection.addEventListener('click',clickEvent);
+let totalClick=25;
 function clickEvent(event){
-    if(event.target.id ==='left' || event.target.id==='middle'|| event.target.id==='right'){
-        for(let i=0;i<BusMall.all.length;i++){
-            if(BusMall.all[i].nameOfProducts===event.target.title){
-                BusMall.all[i].vote++;
-                BusMall.all[i].views++;
-                // consale.table(BusMall.all[i]);
-            }
-        }
-        render();
+  // console.log(event);
+  if(event.target.id ==='left' || event.target.id==='middle'||event.target.id==='right'){
+    totalClick-=1;
+    for(let i=0;i<BusMall.all.length;i++){
+      if(BusMall.all[i].nameOfProducts===event.target.title){
+        BusMall.all[i].vote++;
+      }
     }
+    if (totalClick===0){
+      imageSection.removeEventListener('click',clickEvent);
+    }
+    render();
+  }
+}
+let buttonResult=document.getElementById('buttonResult');
+buttonResult.addEventListener('click',clickButton);
+function clickButton(){
+  let unolderList=document.getElementById('ulList');
+  for (let j=0;j<BusMall.all.length;j++){
+    let firstli = document.createElement('li');
+    firstli.innerText='This product  ' +BusMall.all[j].nameOfProducts+' has '+BusMall.all[j].vote+' votes and '+BusMall.all[j].views+' views .';
+    unolderList.appendChild(firstli);
+    console.log(BusMall.all[j].vote,BusMall.all[j].views);
+  }
+}
 
-}
 function randomNumber(min, max){
-    return Math.floor(Math.random()*(max-min+1))+min;
+  return Math.floor(Math.random()*(max-min+1))+min;
 }
+// function check(){
+//   if (BusMall.all[rightProduct]!= BusMall.all[leftProduct]&& BusMall.all[middleProduct]){
+//   }
+// }
 randomNumber();
 render();
-// const buttonResult=document.getElementById('buttonResult');
-// let totalClick=0;
-// buttonResult.addEventListener('click',clickButton);
-// function clickButton(event){
-//     if(totalClick<25){
-//         alert ()
-//     }
